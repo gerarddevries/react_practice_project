b7520858b32763cef27c1f6b0da3ed1b0a36303b
Gerard de Vries <gerard.devries1968@gmail.com>
2025-03-16 14:01:33 -0400

    fixed bug/typo in HTML return of ConferenceEvent

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -256,7 +256,7 @@ const ConferenceEvent = () => {
             </div>
           ) : (
             <div className="total_amount_detail">
-              <TotalCost totalCosts={totalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
+              <TotalCost totalCosts={avTotalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
             </div>
           )
         }
